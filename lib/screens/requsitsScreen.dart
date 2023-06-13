5597d13d4e5444c7a0d20b88d4f775e20b37819b
Louie Ibrahim <charlotte.for.it.services@gmail.com>
2023-06-13 03:48:12 -0700

    adding sick leve functioncanlty and handle the diffrent between timeoff requsits and the regulare requsist

--- lib/screens/requsitsScreen.dart
+++ lib/screens/requsitsScreen.dart
@@ -1,9 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobilehrmss/models/Dialog.dart';
+import 'package:mobilehrmss/models/yesNoDialog.dart';
+import 'package:mobilehrmss/screens/homeScreen.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 import '../models/AppColors.dart';
+import '../widgets/buildDonutChart.dart';
+import '../widgets/button2.dart';
 
 class requsitsScreen extends StatefulWidget {
   static const routeName = '/requsitsScreen';
@@ -15,12 +21,132 @@ class requsitsScreen extends StatefulWidget {
 }
 
 class _requsitsScreenState extends State<requsitsScreen> {
-   Map<String, String> requists = {};
-
+  Map<String, String> requists = {};
   String selectedOption = '';
+  int counter = 1;
+  bool isLoading = false;
+  List<String> timeOffTitiles = [];
+  Map<String, dynamic>? timeOffDetaks = {};
+  DateTime? sDate = null;
+  DateTime? eDate = null;
 
   @override
   Widget build(BuildContext context) {
+    late Widget widgetToDisplay;
+    switch (counter) {
+      case 1:
+        // first screen
+        widgetToDisplay = Container(
+            width: MediaQuery.of(context).size.width - 50,
+            height: MediaQuery.of(context).size.height - 80,
+            decoration: BoxDecoration(
+                color: Colors.grey.shade200.withOpacity(0.55),
+                borderRadius: BorderRadius.circular(30)),
+            child: SingleChildScrollView(
+              child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SingleChildScrollView(
+                    child: StatefulBuilder(
+                      builder: (BuildContext context, StateSetter setState) =>
+                          Column(
+                        children: [
+                          Column(
+                            children: this
+                                .requists
+                                .keys
+                                .map((e) => ListTile(
+                                      title: Text(e),
+                                      leading: Radio<String>(
+                                        value: e,
+                                        groupValue: selectedOption,
+                                        onChanged: (value) async {
+                                          setState(() {
+                                            selectedOption = value!;
+                                          });
+                                        },
+                                      ),
+                                    ))
+                                .toList(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )),
+            ));
+        break;
+      case 2:
+        // 2.1 let the employee selate the start and end dates and calc the days
+        // 2.2  Check if the the employee has enough days as he requisted if yes send the requsit otherwise display
+        widgetToDisplay = Container(
+          width: MediaQuery.of(context).size.width - 50,
+          height: MediaQuery.of(context).size.height - 50,
+          decoration: BoxDecoration(
+              color: Colors.grey.shade200.withOpacity(0.55),
+              borderRadius: BorderRadius.circular(30)),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                DonutChartWidget(
+                  totalDays: timeOffDetaks!["duration"] != null
+                      ? double.parse(timeOffDetaks!["duration"])
+                      : 0.0,
+                  usedDays: timeOffDetaks!["consume"] != null
+                      ? double.parse(timeOffDetaks!["consume"])
+                      : 0.0,
+                  txt: timeOffDetaks!["title"] != null
+                      ? timeOffDetaks!["title"]
+                      : "error",
+                  color: Colors.blue,
+                ),
+                Container(
+                  child: SfDateRangePicker(
+                    onSelectionChanged: _onSelectionChanged,
+                    selectionMode: DateRangePickerSelectionMode.range,
+                  ),
+                ),
+               SizedBox(height: 15,),
+               Container(
+                 width: 150,
+                 height: 50,
+                 decoration: BoxDecoration(
+                   color: AppColors.color1,
+                   borderRadius: BorderRadius.circular(30)
+                 ),
+                 child: Padding(
+                     padding: EdgeInsets.all(18),
+                     child: Row(
+                         mainAxisAlignment: MainAxisAlignment.center,
+                         children: [Icon(Icons.upload,color: Colors.white,) , SizedBox(width: 10,) , Text('upload' , style: TextStyle(color: Colors.white),)])),
+               ),
+                SizedBox(height: 150,),
+
+              ],
+            ),
+          ),
+        );
+        break;
+      case 3:
+        widgetToDisplay = Container(
+          width: MediaQuery.of(context).size.width - 50,
+          height: MediaQuery.of(context).size.height - 80,
+          decoration: BoxDecoration(
+              color: Colors.grey.shade200.withOpacity(0.55),
+              borderRadius: BorderRadius.circular(30)),
+          child: Center(
+            child: Text(selectedOption,
+                style: TextStyle(color: Colors.white, fontSize: 18)),
+          ),
+        );
+        break;
+
+      default:
+        widgetToDisplay = Container(
+          child: Center(
+            child: Text('error'),
+          ),
+        );
+        break;
+    }
     return Scaffold(
       body: Stack(alignment: Alignment.center, children: [
         Container(
@@ -32,7 +158,7 @@ class _requsitsScreenState extends State<requsitsScreen> {
           ),
         ),
         FutureBuilder(
-            future: getWorkflowData(),
+            future: initt(),
             builder: (ctx, snapShot) {
               if (snapShot.connectionState == ConnectionState.waiting) {
                 return Center(
@@ -45,65 +171,135 @@ class _requsitsScreenState extends State<requsitsScreen> {
                 );
               }
 
-              return Positioned(
-                bottom: 20,
-                child: Container(
-                    width: MediaQuery.of(context).size.width - 50,
-                    height: MediaQuery.of(context).size.height - 80,
-                    decoration: BoxDecoration(
-                        color: Colors.grey.shade200.withOpacity(0.55),
-                        borderRadius: BorderRadius.circular(30)),
-                    child: SingleChildScrollView(
-                      child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: SingleChildScrollView(
-                            child: StatefulBuilder(
-                              builder: (BuildContext context,
-                                      StateSetter setState) =>
-                                  Column(
-                                    children: [
-                                      Column(
-                                children: this
-                                        .requists
-                                        .keys
-                                        .map((e) => ListTile(
-                                              title: Text(e),
-                                              leading: Radio<String>(
-                                                value: e,
-                                                groupValue: selectedOption,
-                                                onChanged: (value) async {
-                                                  setState(() {
-                                                    selectedOption = value!;
-                                                  });
-                                                },
-                                              ),
-                                            ))
-                                        .toList(),
-                              ),
-                                      SizedBox(height: 100),
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          GestureDetector(
-                                              onTap: (){},
-                                              child: Text('Next' , style: TextStyle( fontSize: 18 ),))
-                                        ],
-                                      )
-                                    ],
+              return Container(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    widgetToDisplay,
+                    isLoading
+                        ? Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : Positioned(
+                            bottom: 10,
+                            child: GestureDetector(
+                                onTap: () async {
+                                  // this button behaviour will change depend on the coundter var
+                                  switch (this.counter) {
+                                    case 1:
+                                      //1. Check if the selected requist is from timeOff category if yes go to 2 else go to 3
+                                      if (this.selectedOption.isEmpty) {
+                                        MyDialog.showAlert(context,
+                                            'Before proceeding, please ensure that you have selected the necessary requisites and then try again!');
+                                        return;
+                                      }
+                                      // check if the requist is timeOff type or not
+                                      this
+                                              .timeOffTitiles
+                                              .contains(selectedOption)
+                                          ? this.counter = 2
+                                          : this.counter = 3;
+                                      this.timeOffDetaks = await checkHoliday(
+                                          FirebaseAuth
+                                              .instance.currentUser!.uid,
+                                          this.selectedOption);
+                                      // 2. Check if the employee has this time off , if yes go to 2.1 else display msg saying please contact your HR
+                                      // to assigen this type of timeOff
+                                      if (timeOffDetaks == null &&
+                                          this.counter != 3) {
+                                        MyDialog.showAlert(context,
+                                            "Unfortunately, you do not have this type of time off in your profile. Please contact your HR to request the assignment of this time off and try again later.");
+                                        this.counter = 1;
+                                        return;
+                                      }
+                                      setState(() {});
+                                      break;
+                                    case 2:
+                                    // 2.2  Check if the the employee has enough days as he requisted if yes send the requsit otherwise display
+                                    // you don't have enough days in your time off
+                                     if (this.eDate != null && this.sDate != null)
+                                       {
+                                         // calc how many days do the emplyee requisted
+                                         int requsitedDayes =  this.sDate!.difference(this.eDate!).inDays.abs() + 1 ;
+                                         // calc the avalable days
+                                         double avalableDays =  double.parse(timeOffDetaks!["consume"]) - double.parse(timeOffDetaks!["duration"]) ;
+                                         avalableDays = avalableDays.abs();
+                                         if(requsitedDayes <= avalableDays){
+                                           print('abbrove');
+                                           return;
+                                         }
+                                         else {
+                                          MyDialog.showAlert(context, 'The number of days off you requested exceeds the available days in your profile.') ;
+                                         }
+
+
+                                       }
+
+                                      break;
+                                    case 3:
+                                      //3. send the requist (the non time off requists)
+                                      MyAlertDialog.showConfirmationDialog(
+                                          context,
+                                          'Are you certain about sending the salary certificate request?',
+                                          () async {
+                                        isLoading = true;
+                                        setState(() {});
+                                        await nonTimeOffReq(
+                                            FirebaseAuth
+                                                .instance.currentUser!.uid,
+                                            this.requists[selectedOption]!);
+                                        isLoading = false;
+                                        setState(() {});
+                                        MyDialog.showAlert(context,
+                                            'Your request has been successfully sent.');
+                                      }, () {
+                                        Navigator.of(context).pop();
+                                      });
+                                      break;
+                                    default:
+                                      break;
+                                  }
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: AppColors.color2,
+                                      borderRadius: BorderRadius.circular(30)),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(12.0),
+                                    child: Text(
+                                      this.counter == 3
+                                          ? 'Submit the requist'
+                                          : 'Next',
+                                      style: TextStyle(
+                                          fontSize: 18, color: Colors.white),
+                                    ),
                                   ),
-                            ),
-                          )),
-                    )),
+                                )),
+                          )
+                  ],
+                ),
               );
             }),
       ]),
     );
   }
 
-  Future<void> newRequist(String userId, String workflowId) async {
+  Future<void> initt() async {
+    // The alogrithem for seding requists
+    // 1. Check if the selected requist is from timeOff category if yes go to 2 else go to 3
+    // 2. Check if the employee has this time off , if yes go to 2.1 else display msg saying please contact your HR
+    // to assigen this type of timeOff
+    // 2.1 let the employee selate the start and end dates and calc the days
+    // 2.2  Check if the the employee has enough days as he requisted if yes send the requsit otherwise display
+    // you don't have enough days in your time off
+    // 3. send the requist
+
+    await getWorkflowData();
+    await getTimeOffTitles();
+  }
+
+  Future<void> nonTimeOffReq(String userId, String workflowId) async {
     try {
-      print(userId + '    userId');
-      print(workflowId + '        workflowId');
       DateTime currentDate = DateTime.now();
       List<String> flowOrder = [];
       // Create the request document data
@@ -113,7 +309,6 @@ class _requsitsScreenState extends State<requsitsScreen> {
         'title': workflowId,
         'flow': {},
         'status': 'pending',
-        'ReqistedDays': "7"
       };
       // Retrieve the workflow document
       DocumentSnapshot workflowSnapshot = await FirebaseFirestore.instance
@@ -172,5 +367,63 @@ class _requsitsScreenState extends State<requsitsScreen> {
       MyDialog.showAlert(context, 'error $e');
       throw Exception('Failed to retrieve workflow data: $e');
     }
+  }
+
+  Future<List<String>> getTimeOffTitles() async {
+    List<String> titles = [];
+
+    try {
+      QuerySnapshot querySnapshot =
+          await FirebaseFirestore.instance.collection('timeOff').get();
+
+      if (querySnapshot.docs.isEmpty) {
+        throw Exception('No documents found in the "timeOff" collection.');
+      }
+
+      querySnapshot.docs.forEach((doc) {
+        if (doc.exists) {
+          dynamic x = doc.data() as Map<String, dynamic>;
+          String title = x['title'];
+          titles.add(title);
+        }
+      });
+      this.timeOffTitiles = titles;
+    } catch (e) {
+      print('Error loading time off titles: $e');
+    }
+
+    return titles;
+  }
+
+  Future<Map<String, dynamic>?> checkHoliday(String uid, String title) async {
+    try {
+      // Access the "TimeOff" sub-collection for the employee with the given UID
+      final QuerySnapshot snapshot = await FirebaseFirestore.instance
+          .collection('Employee')
+          .doc(uid)
+          .collection('TimeOff')
+          .where('title', isEqualTo: title)
+          .get();
+
+      if (snapshot.docs.isEmpty) {
+        return null; // The employee doesn't have the specified time off
+      } else {
+        final timeOffDoc = snapshot.docs.first;
+        final timeOffData = timeOffDoc.data();
+        return timeOffData as Map<String,
+            dynamic>; // Return the attributes of the matching time off document
+      }
+    } catch (e) {
+      print('Error checking holiday: $e');
+      return null; // Return null on error
+    }
+  }
+
+  void _onSelectionChanged(DateRangePickerSelectionChangedArgs args) {
+    // TODO: implement your code here
+    this.sDate = args.value.startDate;
+    this.eDate = args.value.endDate;
+    print(this.sDate.toString() + " " + this.eDate.toString());
+   // print(args.value.startDate.difference(args.value.endDate).inDayes);
   }
 }
